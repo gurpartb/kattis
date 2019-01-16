f5f97faa6c6c924414dc4c2e33aeca1a9c1ef0a7
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-01-16 02:24:04 -0700

    added few comments

--- mr_anaga2.py
+++ mr_anaga2.py
@@ -5,11 +5,18 @@ from sys import stdin
 # u: unique set (string set)
 # d: duplicate set (string set)
 # w: word (string)
-# c: chars indices array (int list size: 26)
+# c: character occurence counter array (int list size: 26)
 # a: ascii value of 'a' (int)
 # j: ascii mapping to c index 'a' -> 0 (int)
 
-def to_int(w):
+def f(w):
+    """
+    This function takes in string 'w' as a parameter.
+    'w' must be a word with all lower case letters and no spaces.
+    It counts the number of occurences of each charater of the
+    alphabet and increments the corresponding index in 'c'.
+    It converts the 'c' array into a single int returns it to the caller.
+    """
     c = [0]*26
     a = 97
     for i in w:
@@ -23,7 +30,7 @@ u = set()
 d = set()
 for i in range(n):
     w = stdin.readline()
-    w = to_int(w)
+    w = f(w)
     if w not in d:
         if w in u:
             u.remove(w)
