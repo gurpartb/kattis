354c556108eba8844e0f088c17d4fce86f0b1e14
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-01-16 02:08:01 -0700

    cleaned up the code, and added var description.

--- mr_anaga.py
+++ mr_anaga.py
@@ -1,8 +1,14 @@
 from sys import stdin
 
+# n: number of lines (int)
+# k: length of a word (int)
+# u: unique set (string set)
+# d: duplicate set (string set)
+# w: word (string)
+
 n, k = [int(x) for x in stdin.readline().split()]
-u= set()
-d= set()
+u = set()
+d = set()
 for i in range(n):
     w = stdin.readline()
     w = "".join(sorted(w))
--- mr_anaga2.py
+++ mr_anaga2.py
@@ -1,17 +1,26 @@
 from sys import stdin
 
+# n: number of lines (int)
+# k: length of a word (int)
+# u: unique set (string set)
+# d: duplicate set (string set)
+# w: word (string)
+# c: chars indices array (int list size: 26)
+# a: ascii value of 'a' (int)
+# j: ascii mapping to c index 'a' -> 0 (int)
+
 def to_int(w):
-    wn = [0]*26
-    av = 97
+    c = [0]*26
+    a = 97
     for i in w:
-        wi = ord(i)-av
-        if wi >= 0:
-            wn[wi] += 1
-    return int(''.join(map(str, wn)))
+        j = ord(i)-a
+        if j >= 0:
+            c[j] += 1
+    return int(''.join(map(str, c)))
 
 n, k = [int(x) for x in stdin.readline().split()]
-u= set()
-d= set()
+u = set()
+d = set()
 for i in range(n):
     w = stdin.readline()
     w = to_int(w)
