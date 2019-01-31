8b53fdfe751e309adf0e05beb400951140a7a344
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-01-30 22:13:51 -0700

    added comments

--- mr_anaga4.py
+++ mr_anaga4.py
@@ -1,10 +1,15 @@
 from sys import stdin
 
 def f(w):
-    d = { 'a' : 2 , 'b' : 3 , 'c' : 5 , 'd' : 7 , 'e' : 11 , 'f' : 13 , 'g' : 17 , 'h' : 19 , 'i' : 23 , 'j' : 29 , 'k' : 31 , 'l' : 37 , 'm' : 41 , 'n' : 43 , 'o' : 47 , 'p' : 53 , 'q' : 59 , 'r' : 61 , 's' : 67 , 't' : 71 , 'u' : 73 , 'v' : 79 , 'w' : 83 , 'x' : 89 , 'y' : 97 , 'z' : 101, '\n':1 }
+    """
+    p is the 2,000,000th prime
+    d is dictionary maping or char to prime
+    """
+    d = {'a':2, 'b':3, 'c':5, 'd':7, 'e':11, 'f':13, 'g':17, 'h':19, 'i':23, 'j':29, 'k':31, 'l':37, 'm':41, 'n':43, 'o':47, 'p':53, 'q':59, 'r':61, 's':67, 't':71, 'u':73, 'v':79, 'w':83, 'x':89, 'y':97, 'z':101, '\n':1}
     n = 1
+    p = 32452843
     for i in w:
-        n = (n * d[i]) % 32452843
+        n = (n * d[i]) % p
     return n
 
 n, k = [int(x) for x in stdin.readline().split()]
