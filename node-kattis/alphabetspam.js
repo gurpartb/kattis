80e2940fea6db7fb64901fddfa8b01680ef96a6a
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-02-27 16:11:39 -0700

    finished alphabetspam

---
+++ node-kattis/alphabetspam.js
@@ -0,0 +1,62 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const isSpace = function(x){
+    let _space = 95;
+    return x.charCodeAt(0) === _space;
+}
+
+const isCapital = function(x='A'){
+    let A = 65;
+    let Z = 90;
+    return A <= x.charCodeAt(0) && x.charCodeAt(0) <= Z;
+}
+
+const isReg = function(x){
+    let a = 97;
+    let z = 122;
+    return a <= x.charCodeAt(0) && x.charCodeAt(0) <= z;
+}
+
+const counter = {
+    'space': 0,
+    'reg': 0,
+    'cap': 0,
+    'other': 0
+};
+
+rl.on('line', (line)=>{
+
+    line.split('').forEach(x=>{
+
+        if(isSpace(x)){
+
+            counter.space++;
+
+        } else if(isCapital(x)){
+
+            counter.cap++;
+
+        } else if (isReg(x)){
+
+            counter.reg++;
+
+        } else {
+
+            counter.other++;
+
+        }
+
+    });
+
+    for (const val of Object.values(counter)) {
+
+        console.log(val/line.length);
+
+    };
+
+});
