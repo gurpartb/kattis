bf45f78c123a0344737b196e95260b54b1fabd7d
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-03-09 12:36:58 -0700

    cleaned code

--- node-kattis/imagedecoding.js
+++ node-kattis/imagedecoding.js
@@ -1,8 +1,7 @@
-// * * * time limit exceeded *** //
-
 const readline = require('readline')
 
 const rl = readline.createInterface({
+
     input: process.stdin,
     output: process.stdout
 })
@@ -13,14 +12,17 @@ const dot = '.'
 var tot_lines = 0
 var first_line = true
 
-function main(line){
+rl.on('line', line =>{
+
     if(tot_lines === 0){
 
         tot_lines = parseInt(line)
 
         if(!first_line && tot_lines !== 0){
-            process.stdout.write('\n')
+
+            console.log('')
         }
+        
         first_line = false
 
     } else {
@@ -29,17 +31,22 @@ function main(line){
         let arr = line.split(' ')
         let pixel = arr.shift()
         let pixel_count = 0
-
         let str = ''
 
         arr.forEach( x =>{
+
             for(let i = 0; i < parseInt(x); i++){
+
                 str += pixel
                 pixel_count++
             }
+
             if(pixel === hash){
+
                 pixel = dot
+
             } else {
+
                 pixel = hash
             }
         })
@@ -52,17 +59,13 @@ function main(line){
         }
 
         if(tot_lines === 0){
+
             if(sizes.length > 2){
+
                 console.log('Error decoding image')
             }
+
             sizes = [0]
         }
     }
-
-}
-
-rl.on('line', line =>{
-
-    main(line)
-    
 })
