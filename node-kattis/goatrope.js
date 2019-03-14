4d9760bacd0f6eb43f1d23671555fbaed8c1e137
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-03-14 15:19:42 -0600

    cleaned code layout

--- node-kattis/goatrope.js
+++ node-kattis/goatrope.js
@@ -1,21 +1,28 @@
 const readline = require('readline')
 
 const rl = readline.createInterface({
+
     input: process.stdin,
     output: process.stdout
 })
 
 rl.on('line', (line) => {
+
     let vectors = line.split(' ').map(x => parseFloat(x))
+
     console.log(minDis(...vectors))
 })
 
 // v1 and v2 are arrays ('vectors') of same size
 function dis(v1, v2){
+
     let dis_square = 0
+
     for(let i = 0; i < v1.length; i++){
+
         dis_square += (v1[i]-v2[i])*(v1[i]-v2[i])
     }
+
     return Math.sqrt(dis_square)
 }
 
@@ -48,24 +55,11 @@ function minDis(xg, yg, x1, y1, x2, y2){
 
         return dis([xg, yg], [x2, y2])
     }
-    
+
     if(y1 > yg){
 
         return dis([xg, yg], [x2, y1])
     }
-
-    // if(xg < x1 && yg < y1){
-    //     return dis([xg, yg], [x1, y1])
-    // }
-    // if(xg > x2 && yg > y2){
-    //     return dis([xg, yg], [x2, y2])
-    // }
-    // if(xg < x1 && y2 < yg){
-    //     return dis([xg, yg], [x1, y2])
-    // }
-    // if(xg > x1 && y1 > yg){
-    //     return dis([xg, yg], [x2, y1])
-    // }
 }
 
 // console.log(minDis(...[7, 3, 0, 0, 5, 4] === 2))
