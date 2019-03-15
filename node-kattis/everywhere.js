2294bf506258a4679b730ff80976e900e6cd3728
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-03-15 09:30:51 -0600

    finished everywhere challenge

--- node-kattis/everywhere.js
+++ node-kattis/everywhere.js
@@ -1,24 +1,35 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
+    
     input: process.stdin,
     output: process.stdout
 });
 
 let n
-let k
+let k = 0
 let set = new Set()
+
 rl.on('line', (line) => {
     if(!n){
+
         n = line
+
     } else {
+
         if(!k){
+
             k = parseInt(line)
+
         } else {
-            k -= 0
+
+            k -= 1
             set.add(line)
+
         }
+
         if(!k){
+
             console.log(set.size)
             set.clear()
         }
