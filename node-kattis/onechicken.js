88fad25bce114d1dad59bcd71dc3653731c788a4
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-03-16 11:54:03 -0600

    cleaned up the code

--- node-kattis/onechicken.js
+++ node-kattis/onechicken.js
@@ -1,23 +1,26 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
+
     input: process.stdin,
     output: process.stdout
 });
 
 rl.on('line', (line) => {
+
     var nums = line.split(' ');
     var guests = parseInt(nums[0]);
     var chickens = parseInt(nums[1]);
 
-    var dchicken = Math.abs(guests - chickens);
+    var diff_abs = Math.abs(guests - chickens);
+    var piece_s = (diff_abs === 1)? 'piece': 'pieces';
 
-    var p = (dchicken === 1)? 'piece': 'pieces'
+    if(guests < chickens){
 
-    if( guests < chickens){
-        console.log(`Dr. Chaz will have ${dchicken} ${p} of chicken left over!`)
+        console.log(`Dr. Chaz will have ${diff_abs} ${piece_s} of chicken left over!`);
+
     } else {
-        console.log(`Dr. Chaz needs ${dchicken} more ${p} of chicken!`)
+
+        console.log(`Dr. Chaz needs ${diff_abs} more ${piece_s} of chicken!`);
     }
-    /*Solve the test case and output the answer*/
 });
