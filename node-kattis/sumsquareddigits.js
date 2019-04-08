e1357a80971abb8de4886bbe75505f183cd2b432
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-04-08 17:44:39 -0600

    cleaned up the code

--- node-kattis/sumsquareddigits.js
+++ node-kattis/sumsquareddigits.js
@@ -1,35 +1,10 @@
-var readline = require('readline');
-
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-let first_line;
-
-rl.on('line', line=>{
-
-    if(!first_line){
-
-        first_line = line;
-    }
-    else {
-
-        let arr = line.split(' ');
-        
-        let count = arr[0];
-        let b = parseInt(arr[1]);
-        let n = parseInt(arr[2]);
-
-        let res = SSD(b, n);
-        console.log(count, res);
-    }
-});
-
 function SSD(b, n){
     
     let num_base = n.toString(b);
+
     let arr = num_base.split('');
+
     arr = arr.map(x => parseInt(x,b)**2);
+
     return arr.reduce( (a, b) => a + b, 0);
 }
