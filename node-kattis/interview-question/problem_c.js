ad27e385f2d46bf3c6b5afc38b2ab5323f945092
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-05-20 18:12:16 -0600

    solved problem_c

---
+++ node-kattis/interview-question/problem_c.js
@@ -0,0 +1,106 @@
+// Find last x non-zero digits of n!
+
+const pow_arr = [1];
+
+function pow(base = 12, exp = 16, sig_fig = 3){
+
+    if(pow_arr.length > exp){
+
+        return pow_arr[exp];
+    }
+
+    let res = pow_arr[pow_arr.length - 1];
+    const modder = 10**sig_fig;
+  
+    while(pow_arr.length <= exp){
+
+      res = (res*base)%modder;
+      pow_arr.push(res);
+    }
+  
+    return res;
+}
+  
+function tail_fact(n = 84, sig_fig = 3){
+
+    let res = 1;
+
+    const modder = 10**sig_fig;
+
+    while(n % 5){
+
+        res = (res*n)%modder;
+        n--;
+    }
+
+    return res;
+}
+  
+function factorial(n = 3, sig_fig = 3){
+
+    let res = 1;
+    const modder = 10**sig_fig;
+
+    if(n <= 1){
+
+        return res;
+    }
+
+    while(n > 1){
+
+        res = (res*n)%modder;
+        n--;
+    }
+
+    return res;
+}
+  
+function D(n = 84, sig_fig = 3){
+
+    let res;
+
+    if(n < 5){
+
+        return factorial(n, sig_fig);
+    }
+
+    const exp = Math.floor(n/5);
+    const base = 12;
+    const modder = 10**sig_fig;
+
+    res = (pow(base, exp, sig_fig)*D(exp))%modder;
+    res = (res*tail_fact(n))%modder;
+
+    return res;
+}
+  
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+
+    input: process.stdin,
+    output: process.stdout
+
+});
+
+const sig_fig = 3;
+
+rl.on('line', (line) => {
+
+    const num = parseInt(line);
+
+    let res = D( num, sig_fig);
+
+    if(res <= 99 && num >= 7 ){
+
+        res = res.toString();
+
+        while(res.length < 3){
+
+            res = '0' + res;
+        }
+    }
+
+    console.log(res);
+});
