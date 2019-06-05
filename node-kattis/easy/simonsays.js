03ac1d3c0cab7d734666db3d424ab5428af51040
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-06-04 22:53:31 -0600

    solved simonsays

---
+++ node-kattis/easy/simonsays.js
@@ -0,0 +1,36 @@
+
+
+function simonsays(line = 'Simon sa hello byes'){
+
+    const simon = 'Simon';
+    const says = 'says';
+
+    const arr = line.split(' ');
+
+    return (arr[0] === simon && arr[1] === says)? arr.splice(2,arr.length).join(' ') : null;
+}
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let n;
+rl.on('line', (line) => {
+
+    if(!n){
+
+        n = line;
+    }
+    else{
+
+        const res = simonsays(line);
+
+    if(res){
+        
+        console.log(res);
+    }
+}
+});
