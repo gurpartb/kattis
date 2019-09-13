9a5026adfe9e868f49a26aa2a214266bbf2c7c83
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-09-12 20:14:02 -0600

    solved engineering english

--- engineeringenglish.cs
+++ engineeringenglish.cs
@@ -7,28 +7,22 @@ namespace engeneeringenglish
     {
         static void Main(string[] args)
         {
-            // create a set 'seenItBefore'
             HashSet<string> seenItBefore = new HashSet<string>();
 
             string line;
 
             while( (line = Console.ReadLine()) != null)
             {
-                // parse all words in an array
                 string[] arr = line.Split(' ');
 
-                // sequentially pick words from the array
                 for (int i = 0; i < arr.Length; i++)
                 {
                     string wordLowercase = arr[i].ToLower();
-
-                    // if word.lowercase() in 
+ 
                     if (seenItBefore.Contains(wordLowercase))
                     {
                         Console.Write('.');
                     }
-                    // if word.lowercase() is NOT in the array print the word
-                    // add word.lowercase() to the 'seenItBefore' set
                     else
                     {
                         Console.Write(arr[i]);
