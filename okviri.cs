d57450a90ef4ba661200ebb69044f5dd4e42038f
gurpartb <gurpartap.s.bhatti@gmail.com>
2020-04-01 17:10:38 -0600

    added few changes

--- okviri.cs
+++ okviri.cs
@@ -7,43 +7,47 @@ namespace okviri
         public static void Main(string[] args)
         {
             string[] lineOut = new string[3]{".",".","#"};
-            
+
             string line = Console.ReadLine();
 
             int i = 0;
             while(i < line.Length)
             {
-                if(i % 3 == 2)
+                if(i % 3 == 0)
+                {
+                    lineOut[0] += Line0a();
+                    lineOut[1] += Line1a();
+                    lineOut[2] += Line2a(line[i]);
+                }
+                else if(i % 3 == 1)
+                {
+
+                    lineOut[0] += Line0a();
+                    lineOut[1] += Line1a();
+
+                    // if its the last letter close line, with "#" else with "*"
+                    lineOut[2] += (i == line.Length - 1)? Line2a(line[i]) : Line2b(line[i]);
+                }
+                else
                 {
                     lineOut[0] += Line0b();
                     lineOut[1] += Line1b();
                     lineOut[2] += Line2b(line[i]);
                 }
-                else
-                {
-                    lineOut[0] += Line0a();
-                    lineOut[1] += Line1a();
-
-                    if(i < line.Length - 1 && i % 3 == 1)
-                    {
-                        lineOut[2] += Line2b(line[i]);
-                    }
-                    else
-                    {
-                        lineOut[2] += Line2a(line[i]);
-                    }
-                }
                 i++;
             }
 
-            foreach(string str in lineOut)
-            {
-                System.Console.WriteLine(str);
-            }
-
-            for (int j = lineOut.Length - 2; j >= 0; j--)
+            // print 0 to 1 then 2 back to 0;
+            int j = 0;
+            while(j < lineOut.Length - 1)
             {
                 System.Console.WriteLine(lineOut[j]);
+                j++;
+            }
+            while(j >= 0)
+            {
+                System.Console.WriteLine(lineOut[j]);
+                j--;
             }
         }
 
