0947425341739431083f90fa2bf956795311a959
gurpartb <gurpartap.s.bhatti@gmail.com>
2020-04-10 21:37:22 -0600

    solved loo rolls

---
+++ loorolls.cs
@@ -0,0 +1,35 @@
+// Kattis : Loo Rolls
+
+using System;
+
+namespace loorolls
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            long[] ln = StringToIntArray(line);
+
+            long count = 0;
+
+            long r = ln[0] % ln[1];
+            count++;
+
+            while(r > 0)
+            {
+                ln[1] += -r;
+                r = ln[0] % ln[1];
+                count++;
+            }
+
+            System.Console.WriteLine(count);
+        }
+
+        static long[] StringToIntArray(string line)
+        {
+            long[] arr = Array.ConvertAll(line.Split(' '), long.Parse);
+            return arr;
+        }
+    }
+}
