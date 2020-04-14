d6b93c12df7815232d7f01365509c378be80d0be
gurpartb <gurpartap.s.bhatti@gmail.com>
2020-04-13 23:46:01 -0600

    cleaned up the code

--- permutedarithmeticsequence.cs
+++ permutedarithmeticsequence.cs
@@ -13,27 +13,34 @@ namespace permutedaritmeticsequence
             while( (line = Console.ReadLine()) != null)
             {
                 int[] arr = Array.ConvertAll(line.Split(' '), int.Parse);
-                int[] arr1 = new int[arr.Length - 1];
+                arr = SliceArray(arr, 1, arr.Length);
 
-                int i = 1;
-                while(i < arr.Length)
-                {
-                    arr1[i-1] = arr[i];
-                    i++;
-                }
-
-                if(IsArithmetic(arr1))
+                if(IsArithmetic(arr))
                 {
                     System.Console.WriteLine("arithmetic");
                     continue;
                 }
-                if(IsPermutedArithmetic(arr1))
+
+                if(IsPermutedArithmetic(arr))
                 {
                     System.Console.WriteLine("permuted arithmetic");
                     continue;
                 }
+                
                 System.Console.WriteLine("non-arithmetic");
             }
+        }
+
+        static int[] SliceArray(int[] arr, int start, int end)
+        {
+            int i = 1;
+            int[] arr1 = new int[end-start];
+            while(i < end)
+            {
+                arr1[i-1] = arr[i];
+                i++;
+            }
+            return arr1;
         }
 
         static bool IsArithmetic(int[] arr)
@@ -43,7 +50,7 @@ namespace permutedaritmeticsequence
             {
                 int diff1 = arr[i - 1] - arr[i - 2];
                 int diff2 = arr[i] - arr[i - 1];
-                
+
                 if(diff1 != diff2)
                 {
                     return false;
