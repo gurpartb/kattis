dd35bb9c33fa470a9f8fd2ebcc806f2916df936e
gurpartb <gurpartap.s.bhatti@gmail.com>
2020-05-24 21:20:37 -0600

    solved misa

--- misa.cs
+++ misa.cs
@@ -9,87 +9,53 @@ namespace misa
         {
             int[] rowCol = ReadIntArray();
             string[] map = ReadStringArray(rowCol[0]);
-            int n = HandShakesTotal(map,'o','o',(_n,_m)=> _n + _m);
-            int m = HandShakesTotal(map,'.','o',(_n,_m)=> Math.Max(_n,_m));
-            System.Console.WriteLine(n/2 + m);
+            int n = HandShakesTotal(map);
+            System.Console.WriteLine(n);
         }
 
-        static int HandShakesTotal(string[] map, char individual, char neighbor, Func<int,int,int> f)
+        static int HandShakesTotal(string[] map)
         {
             int n = 0;
+            int m = 0;
+
+            char individual = 'o';
+            char neighbor = 'o';
+
             for (int i = 0; i < map.Length; i++)
             {
                 for (int j = 0; j < map[i].Length; j++)
                 {
                     if(map[i][j] == individual)
                     {
-                        n = f(n, HandShakesIndividual(map, i, j, neighbor));
+                        n += HandShakesIndividualSE(map, i, j, neighbor);
+                    }
+                    else
+                    {
+                        m = Math.Max(
+                                m,
+                                HandShakesIndividualSE(map, i, j, neighbor) +
+                                HandShakesIndividualNW(map, i, j, neighbor)
+                            );
                     }
                 }
             }
-            return n;
+
+            return n + m;
         }
 
-        static int HandShakesIndividual(string[] map, int i, int j, char neighbor)
+        static int HandShakesIndividualSE(string[] map, int i, int j, char neighbor)
         {
             int n = 0;
-            
-            bool north = i > 0;
-            bool east = map[i].Length > j + 1;
+
             bool south = map.Length > i + 1;
+            bool east = map[i].Length > j + 1;
             bool west = j > 0;
 
-            int ni = i - 1;
-            int ej = j + 1;
-            int si = i + 1;
             int wj = j - 1;
+            int si = i + 1;
+            int ej = j + 1;
 
             Func<int,int,bool> isNeighbor = (_i,_j) => map[_i][_j] == neighbor;
-
-            // N
-            if(north)
-            {
-                if(isNeighbor(ni, j))
-                {
-                    n++;
-                }
-
-                // NW
-                if(west)
-                {
-                    if(isNeighbor(ni, wj))
-                    {
-                        n++;
-                    }
-                }
-
-                // NE
-                if(east)
-                {
-                    if(isNeighbor(ni,ej))
-                    {
-                        n++;
-                    }
-                }
-            }
-
-            // W
-            if(west)
-            {
-                if(isNeighbor(i, wj))
-                {
-                    n++;
-                }
-            }
-
-            // E
-            if(east)
-            {
-                if(isNeighbor(i, ej))
-                {
-                    n++;
-                }
-            }
 
             // s
             if(south)
@@ -97,15 +63,6 @@ namespace misa
                 if(isNeighbor(si, j))
                 {
                     n++;
-                }
-
-                // sw
-                if(west)
-                {
-                    if(isNeighbor(si, wj))
-                    {
-                        n++;
-                    }
                 }
 
                 // se
@@ -116,7 +73,79 @@ namespace misa
                         n++;
                     }
                 }
+
+                // sw
+                if(west)
+                {
+                    if(isNeighbor(si, wj))
+                    {
+                        n++;
+                    }
+                }
             }
+
+            // e
+            if(east)
+            {
+                if(isNeighbor(i, ej))
+                {
+                    n++;
+                }
+            }
+
+            return n;
+        }
+
+        static int HandShakesIndividualNW(string[] map, int i, int j, char neighbor)
+        {
+            int n = 0;
+
+            bool north = i > 0;
+            bool west = j > 0;
+            bool east = map[i].Length > j + 1;
+
+            int ej = j + 1;
+            int ni = i - 1;
+            int wj = j - 1;
+
+            Func<int,int,bool> isNeighbor = (_i,_j) => map[_i][_j] == neighbor;
+
+            // n
+            if(north)
+            {
+                if(isNeighbor(ni, j))
+                {
+                    n++;
+                }
+
+                // ne
+                if(east)
+                {
+                    if(isNeighbor(ni, ej))
+                    {
+                        n++;
+                    }
+                }
+
+                // nw
+                if(west)
+                {
+                    if(isNeighbor(ni, wj))
+                    {
+                        n++;
+                    }
+                }
+            }
+
+            // w
+            if(west)
+            {
+                if(isNeighbor(i, wj))
+                {
+                    n++;
+                }
+            }
+
             return n;
         }
 
